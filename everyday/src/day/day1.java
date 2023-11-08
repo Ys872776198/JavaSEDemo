e3acf8572922f6493b83e564ff4ec71a04d14ac4
YaoSheng <872776198@qq.com>
2023-11-08 22:50:29 +0800

    test2

---
+++ everyday/src/day/day1.java
@@ -0,0 +1,13 @@
+package day;
+
+/**
+ * @Auther: YS
+ * @Date: 2023/11/8 22:49
+ * @Description: IntelliJ IDEA
+ * @Version: 1.0
+ */
+public class day1 {
+    public static void main(String[] args) {
+        System.out.println("hello world!");
+    }
+}
--- pers/src/base/syntax/CommonSyntax.java
+++ pers/src/base/syntax/CommonSyntax.java
@@ -10,7 +10,7 @@ import java.util.Arrays;
  */
 public class CommonSyntax {
     public static void main(String[] args) {
-//        System.out.println("hello world");
+        System.out.println("hello world");
 
         /*冒泡排序*/
             /* 数组的常见定义方法 */
